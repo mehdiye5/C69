6fd2f3c7e6d5063303ffc60c64a53b9c7e178d83
Mehdiyev <greg.mehdiyev@cgi.com>
2019-07-08 16:40:57 -0400

    wrote comments for test.sh

--- Assignment2/starter/test.sh
+++ Assignment2/starter/test.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# progs in the traceprogs directory
 for prog in simpleloop matmul blocked test
 do
+	# run progs on following memory sizes
 	for memory_size in 50 100 150 200
 	do
+		# for each trace algorithms
 		for trace in clock fifo lru opt rand
 		do
+			# run sim.c with following flags
+			# check line 93 to 110 for flag references
 			./sim -f traceprogs/$prog -s 3000 -a $trace -m $memory_size >> output.txt 
 		done 
 	done

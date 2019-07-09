1b9081124957e41be756184c605252b7416d888b
Mehdiyev <greg.mehdiyev@cgi.com>
2019-07-09 09:10:36 -0400

    finished impleamenting fifo.c

--- Assignment2/starter/fifo.c
+++ Assignment2/starter/fifo.c
@@ -12,13 +12,20 @@ extern int debug;
 
 extern struct frame *coremap;
 
+int index;
+
 /* Page to evict is chosen using the fifo algorithm.
  * Returns the page frame number (which is also the index in the coremap)
  * for the page that is to be evicted.
  */
 int fifo_evict() {
+	// index to be evicted
+	int evicted = index;
+
+	// new index location
+	index = (index + 1) % memsize;
 	
-	return 0;
+	return evicted;
 }
 
 /* This function is called on each access to a page to update any information
@@ -34,4 +41,5 @@ void fifo_ref(pgtbl_entry_t *p) {
  * replacement algorithm 
  */
 void fifo_init() {
+	index = 0;
 }

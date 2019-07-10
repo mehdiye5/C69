7d5237e94392b5b82aebb0207844ed63c0baca41
Mehdiyev <greg.mehdiyev@cgi.com>
2019-07-10 15:34:27 -0400

    changed make file

--- Assignment2/starter/traceprogs/queloop.c
+++ Assignment2/starter/traceprogs/queloop.c
@@ -2,15 +2,19 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+//queue object
 typedef struct queu_obj {
     int d[400];
 };
 
+// aray pointer of a point, enougn memory to create queue array
 struct queu_obj** q = malloc(size *sizeof(struct queu_obj));
 
+//populate the array
 void que_loop (int size) {
     int i;
-        
+
+    // go throught the array and populate with a queu struct    
     for (i = 0; i < size; i++) {
         q[i] = malloc(sizeof(struct queu_obj))
         q[i].d = i;    
@@ -18,6 +22,8 @@ void que_loop (int size) {
     return;
 }
 
+
+// free the momory that was alocated to que array
 void empty_queu(int size) {
     int i;
     for (i= 0; i < size; i++) {

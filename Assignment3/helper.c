fc1f305213c4cdf946f5053915bd5ad0eeef8125
Ruiming Xu <ruiming.xu@mail.utoronto.ca>
2019-08-03 14:24:23 -0400

    add parsing helper function

--- Assignment3/helper.c
+++ Assignment3/helper.c
@@ -2,10 +2,11 @@
 This is a helper module where the helper functions will be located, to be used by other programs.
  */
 
- #include <stdio.h>
- #include <libgen.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <libgen.h>
 #include <string.h>
- #include "ext2.h"
+#include "ext2.h"
  
 
  /**
@@ -55,3 +56,42 @@ char* get_file_name(char* directory) {
     char* copy = strdup(directory);    
     return basename(copy);
 }
+
+/**
+ * Function that parse the input path by '/', and return an array of strings,
+ * each represents a directory name from the input path
+ */
+char (*parse_absolute_path(char* inpPath))[EXT2_NAME_LEN] {
+    char *currChar = inpPath + 1; // Skip the first root directory slash '/'
+    char currStr[EXT2_NAME_LEN];
+    int iCurrStr = 0;
+    char tempBox[10][EXT2_NAME_LEN];
+    int iTempBox = 0;
+
+    while (*currChar != '\0') {
+        if (*currChar == '/') {
+            currStr[iCurrStr] = '\0';
+            strcpy(tempBox[iTempBox], currStr);
+            iCurrStr = 0;
+            iTempBox ++;
+            currChar ++;
+            continue;
+        }
+        currStr[iCurrStr] = *currChar;
+        iCurrStr ++;
+        currChar ++;
+    }
+    if (iCurrStr != 0) {
+        currStr[iCurrStr] = '\0';
+        strcpy(tempBox[iTempBox], currStr);
+        iTempBox ++;
+    }
+    char (*resultBox)[EXT2_NAME_LEN] = malloc((iTempBox) * sizeof(char[EXT2_NAME_LEN]));
+    for (int i = 0; i < iTempBox; i ++) {
+        strcpy(resultBox[i], tempBox[i]);
+    }
+    strcpy(resultBox[iTempBox], "\\");
+    
+    return resultBox;
+}
+
--- Assignment3/helper.h
+++ Assignment3/helper.h
@@ -2,6 +2,9 @@
 
 int get_root_inode_number(void);
 struct ext2_group_desc *get_blocks_group_descriptor(unsigned char* disk);
+unsigned char *get_block_bitmap(unsigned char* disk); // added in Robert's branch
+unsigned char *get_inode_bitmap(unsigned char* disk); // added in Robert's branch
 unsigned char  *get_inode_table(unsigned char* disk);
 struct ext2_inode *get_inode(int inode_number, unsigned char* disk);
+char** parse_absolute_path(char* inpPath); // added in Robert's branch
 

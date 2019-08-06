4722db5802ad1f6776dab84c204e16112769f147
RuimingXu <48458683+RuimingXu@users.noreply.github.com>
2019-08-06 19:50:36 -0400

    Update helper.h

--- Assignment3/helper.h
+++ Assignment3/helper.h
@@ -1,4 +1,6 @@
 // This is a header file for the helper.c 
+#define BASE_OFFSET 1024 //#
+#define BLOCK_OFFSET(block) (BASE_OFFSET + (block-1)*EXT2_BLOCK_SIZE) //#
 
 void * unset_inode_bitmap(int inode_number, unsigned char* disk);
 void * unset_block_bitmap(int block_number, unsigned char* disk);
@@ -21,5 +23,33 @@ void empty_inode_block(int block_number, unsigned char *disk);
 void empty_direct_blocks(int inode_number, unsigned char * disk);
 void empty_sigle_indirect_blocks(int inode_number, unsigned char* disk);
 int delete_file_inode(char *directory ,unsigned char *disk);
-
 int delete_file_inode(char *directory ,unsigned char *disk);
+
+unsigned char *get_block_bitmap(unsigned char* disk); //#
+unsigned char *get_inode_bitmap(unsigned char* disk); //#
+
+unsigned char *get_block(int block_number, unsigned char* disk); //#
+
+int match_name(char *path, char* actual, int start, int last); //#
+char** parse_absolute_path(char* inpPath); //#
+
+//#
+typedef struct Three_indices {
+    int anchor;
+    int last_char;
+    int last_dir;
+} Three_indices;
+
+//#
+typedef struct iNode_info {
+    struct ext2_inode *iNode;
+    int iNode_number;
+} iNode_info;
+
+iNode_info *step_to_target(unsigned char* disk, int fd, char *path, int flag); //#
+Three_indices generate_position(char *path); //#
+int find_free_inode(unsigned char *disk); //#
+int find_free_block(unsigned char *disk); //#
+int find_spot_for_inode_entry(int inode_number, unsigned char *disk); //#
+void update_inode_bitmap (int binary_state, int inode_number, unsigned char *disk); //#
+void printInfo(unsigned char *disk); //#
